ef000514e3f0febc8663d43fc77fc9afd2066f47
Freshia Njoki <93515966+Freshia-Njoki@users.noreply.github.com>
2026-08-18 00:11:14 +0300

    Replace AWS provider with Azure provider in Terraform

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.92"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
     }
   }
 
