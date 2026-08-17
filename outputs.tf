b26ae196869fb8c43431263210120a34045cb2f6
Freshia Njoki <93515966+Freshia-Njoki@users.noreply.github.com>
2026-08-18 00:12:22 +0300

    Add Azure resource outputs to outputs.tf
    Added outputs for Azure resources including VM details and network configurations.

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,34 @@
-output "instance_hostname" {
-  description = "Private DNS name of the EC2 instance."
-  value       = aws_instance.app_server.private_dns
+output "resource_group_name" {
+  description = "Name of the Azure resource group."
+  value       = azurerm_resource_group.main.name
 }
 
-output "instance_security_group_ids" {
-  value = aws_instance.app_server.vpc_security_group_ids
+output "vm_name" {
+  description = "Name of the Azure Linux virtual machine."
+  value       = azurerm_linux_virtual_machine.app_server.name
 }
 
-output "instance_subnet" {
-  value = aws_instance.app_server.subnet_id
+output "vm_private_ip" {
+  description = "Private IP address of the Azure VM."
+  value       = azurerm_network_interface.main.private_ip_address
+}
+
+output "vm_public_ip" {
+  description = "Public IP address of the Azure VM."
+  value       = azurerm_public_ip.main.ip_address
+}
+
+output "subnet_id" {
+  description = "ID of the Azure subnet."
+  value       = azurerm_subnet.main.id
+}
+
+output "network_security_group_id" {
+  description = "ID of the Azure network security group."
+  value       = azurerm_network_security_group.main.id
+}
+
+output "ssh_command" {
+  description = "SSH command for connecting to the VM."
+  value       = "ssh ${var.admin_username}@${azurerm_public_ip.main.ip_address}"
 }
